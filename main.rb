c96abc83f8b1d626ed2c8c16814a18baa7d29a5f
dev-4 <dev.4@protonmail.com>
2016-10-10 10:38:37 -0400

    Added layout.erb, .gitignore, minor bug fixes

--- main.rb
+++ main.rb
@@ -1,17 +1,21 @@
 require 'sinatra'
 
 get '/' do
+	@title = "Home - DTL"
 	erb :home	
 end
 
 get '/about' do 
+	@title = "About - DTL"
 	erb :about
 end
 
 get '/contact' do
+	@title = "Contact - DTL"
 	erb :contact
 end
 
 get '/gallery' do
+	@title = "Shop - DTL"
 	erb :gallery
 end
