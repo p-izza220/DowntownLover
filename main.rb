f25d1ee34e2e6666adf805d2b184af02e34c1ede
dev-4 <dev.4@protonmail.com>
2016-10-10 15:01:19 -0400

    Added background and different style for three pages, minor bug fixes

--- main.rb
+++ main.rb
@@ -1,21 +1,52 @@
 require 'sinatra'
 
 get '/' do
+	@style = "css/default.css"
 	@title = "Home - DTL"
 	erb :home	
 end
 
 get '/about' do 
+	@style = "css/style.css"
 	@title = "About - DTL"
 	erb :about
 end
 
 get '/contact' do
+	@style = "css/style.css"
 	@title = "Contact - DTL"
 	erb :contact
 end
 
 get '/gallery' do
+	@style = "css/style.css"
 	@title = "Shop - DTL"
 	erb :gallery
+end
+
+post '/contact' do
+  mail = SendGrid::Mail.new( 
+    SendGrid::Email.new(email: "dev4mc@gmail.com"),
+    "Thanks for contacting Downtown Lover",
+    SendGrid::Email.new(email: params[:email] ),
+    SendGrid::Content.new(type: 'text/plain', value: <<-EMAILCONTENTS
+      Thanks for letting us know how you feel.
+
+      Our team will be in contact with you shortly.  For your records here's a copy of the feedback we recieved:
+---------------------------------
+      #{params[:message]}
+EMAILCONTENTS
+      )
+  )
+  sg = SendGrid::API.new( api_key: ENV['SENDGRID_API_KEY'] )
+
+  response = sg.client.mail._('send').post(request_body: mail.to_json)
+
+  puts response.status_code
+  puts response.body
+  puts response.headers
+
+  @title = "Contact XYZ"
+  @msg = "Thanks for your submission"
+  erb :contact
 end
